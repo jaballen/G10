d1a8a35b2bbc863a1de59fa186fba20405d321f3
elichu <elichu0@gmail.com>
2016-03-28 01:03:29 -0700

    removed .html, restored rewards.php

--- rewards.php
+++ rewards.php
@@ -127,26 +127,35 @@
 				</div>
 				<div class="address">
 				<p>1154 Robson St<br>Vancouver, BC<br>V6E 1B5<br>(778)123-4567<br><br>OPEN 10:00 - 8:00<br>CLOSED WEDNESDAYS</p>
+				<?php
+						if (isLoggedIn()){
+							echo '<p id="loggedin">Hello, '.$_SESSION['SESS_FIRST_NAME'].' <br/> <a href="./logout.php">Logout</a><br/></p>';
+						} 
+				?>				
 				</div>
 			</div>
 
 			<!-- div navigation -->
 			<div id="nav">
 				<ul>
-					<li><a href="index.html">Home</a></li>
-					<li><a href="about.html">About</a>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="about.php">About</a>
 						<ul>
-							<li><a href="about.html">Our Story</a></li>
-							<li><a href="location_contact.html">Location</a></li>
+							<li><a href="about.php">Our Story</a></li>
+							<li><a href="location_contact.php">Location</a></li>
 						</ul>
 					</li>
-					<li><a href="gallery.html">Gallery</a></li>
-					<li><a href="order.html">Order</a></li>
-					<li><a href="menu.html">Menu</a></li>
-					<li><a class="active" href="rewards.html">Rewards</a>
+					<li><a href="gallery.php">Gallery</a></li>
+					<li><a href="order.php">Order</a></li>
+					<li><a href="menu.php">Menu</a></li>
+					<li><a class="active" href="rewards.php">Rewards</a>
 						<ul>
-							<li><a href="login.html">Log In</a></li>
-							<li><a href="rewards.html">Sign Up</a></li>
+						<?php if (isLoggedIn()) { 
+							echo '<li><a href="members.php">Member Area</a></li>';
+							} else {							
+							echo '<li><a href="login.php">Log In</a></li>';
+							echo '<li><a href="rewards.php">Sign Up</a></li>';
+						} ?>
 						</ul>
 					</li>
 				</ul>
@@ -188,15 +197,15 @@
 						<br>
 						<label for="password">PASSWORD</label>
 						<br>
-						<input type="password" id="password1" name="password" onblur="ValidateForm()"><span id="match"></span>
+						<input type="password" id="password1" name="password1" onblur="ValidateForm()"><span id="match"></span>
 						<br><span id="pwInvalid" style="display: none">&nbsp;Must be 4-8 characters, with at least one digit, and one uppercase letter</span><br>
 						<label for="password">CONFIRM PASSWORD</label>
 						<br>
-						<input type="password" id="password2" name="password" onblur="ValidateForm()"><span id="nomatch"></span>
+						<input type="password" id="password2" name="password2" onblur="ValidateForm()"><span id="nomatch"></span>
 						<br>
 						<input type="submit">
 						<br>
-						<a href="login.html">Already a member?</a>
+						<a href="login.php">Already a member?</a>
 					</form>
 					</div>
 				</div>
@@ -204,7 +213,7 @@
 
 			<!-- div footer -->
 			<div id="footer">
-				<p>© 2016 | Cupcakes on a Cloud | <a href="sitemap.html">Sitemap</a></p>
+				<p>© 2016 | Cupcakes on a Cloud | <a href="sitemap.php">Sitemap</a></p>
 				
 				<!-- div socialbuttons -->
 				<div class="socialbuttons">
